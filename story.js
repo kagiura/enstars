a9cf9ef3432c8377a54e9bd4268ab00bc6569e14
jeaoq <re.jeaoq@gmail.com>
2021-07-02 06:25:00 +0700

    .

--- story.js
+++ story.js
@@ -63,6 +63,10 @@ var icons = document.createElement('link');
 icons.href = 'https://fonts.googleapis.com/icon?family=Material+Icons+Round';
 icons.rel = 'stylesheet';
 document.head.appendChild(icons);
+var cssStyle = document.createElement('link');
+cssStyle.href = 'https://jeaoq.github.io/enstars-wiki/story-options.css';
+cssStyle.rel = 'stylesheet';
+document.head.appendChild(cssStyle);
 
 $('.mw-parser-output .storyNavBar:first-child > tbody > tr > *:nth-child(2)').after('<th colspan="2" class="story-resize-img"><a onclick="resizeImg();"><span class="material-icons-round">question_answer</span></a></th><th colspan="2" class="story-resize-text"><a><span class="material-icons-round">text_fields</span></a><ul><li id="sf-14" onclick="storyOptionsFontSize(\'14\');">14px</li><li id="sf-16" onclick="storyOptionsFontSize(\'16\');">16px</li><li id="sf-18" onclick="storyOptionsFontSize(\'18\');">18px</li><li id="sf-21" onclick="storyOptionsFontSize(\'21\');">21px</li></ul></th><th class="story-bookmark"><a href="#bookmark" onclick="bookmark();"><span class="material-icons-round">bookmark_border</span></a></th>');
 $(document.querySelector('.storyNavBar')).addClass('storyTopNav');
