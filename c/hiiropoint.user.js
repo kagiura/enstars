6a621d2b0cb70d59f0e6de2e91b3a4ff558a8204
kyuu <58155530+jeaoq@users.noreply.github.com>
2021-09-17 11:48:03 +0700

    hp

---
+++ c/hiiropoint.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         HiiroPoint
+// @namespace    https://tsukina.ga/leo
+// @version      1.0
+// @description  :HiiroPoint:
+// @author       twitter.com/junsweats
+// @match        https://twitter.com/*
+// @match        https://*.twitter.com/*
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @grant        none
+// ==/UserScript==
+
+var jQ = document.createElement('script');
+jQ.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+document.head.appendChild(jQ);
+
+const emotes = [];
+var emotesLoaded = false;
+
+function DOMModificationHandler(){
+    $(this).unbind('DOMSubtreeModified.event1');
+    setTimeout(function(){
+        if(emotesLoaded){
+            pageUpdate();
+        }
+        console.log('update!');
+        $('body').bind('DOMSubtreeModified.event1', DOMModificationHandler);
+    },10);
+}
+$('body').bind('DOMSubtreeModified.event1', DOMModificationHandler);
+
+$(function() {
+    $('head').append(`<style>@import url("https://r.tsukina.ga/hp/emotes.css");</style>`)
+    $.getJSON('https://r.tsukina.ga/hp/emotes.json', function(data) {
+        emotes = data;
+        emotesLoaded = true;
+        pageUpdate();
+    });
+});
+
+function pageUpdate(){
+
+    $.expr[":"].contains = $.expr.createPseudo(function(arg) {
+        return function( elem ) {
+            return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+        };
+    });
+
+    var page = $('body').html();
+    for (const val of emotes) {
+        console.log(`span:contains("${val}")`);
+        $(`span:contains("${val}")`).each(function() {
+            console.log(this);
+            var re = new RegExp(`:${val}:`,"gi");
+            $(this).html( $(this).html().replace(re, `<i class="hp-icon hp-${val.toLowerCase()}"></i>`) );
+        })
+        console.log(val);
+    }
+
+    $(`span:contains("@junsweats")`).each(function() {
+        var re = new RegExp(`@junsweats`,"gi");
+        $(this).html( $(this).html().replace(re, `@<i class="hp-icon hp-junsweat"></i>`) );
+    })
+}
